deb6823a7908b49ed64f985103e3460bda18dffe
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-29 20:16:08 +0700

    feat(search page): создать страницу поиска видео

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Search from '@/views/Search'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Search',
+    component: Search
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Search.vue
@@ -0,0 +1,15 @@
+<template>
+	<app-search-form />
+</template>
+
+<script>
+import AppSearchForm from '@/components/AppSearchForm'
+
+export default {
+	name: 'Search',
+
+	components: {
+		AppSearchForm,
+	},
+}
+</script>
