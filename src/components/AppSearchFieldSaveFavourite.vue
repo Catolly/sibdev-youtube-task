04e8afd0113a2fd9b44a7dbe5f687817259f728e
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-30 01:31:09 +0700

    feat(search field save favourite): расширить search field base кнопкой добавления в избранное

---
+++ src/components/AppSearchFieldSaveFavourite.vue
@@ -0,0 +1,39 @@
+<template>
+	<app-search-field-base>
+		<v-menu 
+			:close-on-content-click="false"
+			offset-y 
+			max-width="220"
+			nudge-left="100"
+		>
+			<template v-slot:activator="{ on }">
+				<v-icon
+					color="primary"
+					v-on="on"
+				>
+					mdi-heart-outline
+				</v-icon>
+			</template>
+			<v-card>
+				<v-card-text class="black--text">
+					Поиск сохранён в разделе «Избранное»
+				</v-card-text>
+				<v-card-text class="pt-0">
+					<router-link to="favourites">Перейти в избранное</router-link>
+				</v-card-text>
+			</v-card>
+		</v-menu>
+	</app-search-field-base>
+</template>
+
+<script>
+import AppSearchFieldBase from '@/components/AppSearchFieldBase'
+
+export default {
+	name: 'AppSearchFieldSaveFavourite',
+
+	components: {
+		AppSearchFieldBase,
+	},
+}
+</script>
