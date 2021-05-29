9b66fffaa598d602d628183f86234180c65a9a80
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-30 01:32:48 +0700

    feat(results search form): создать форму поиска видео с сохранением запроса в избранное

---
+++ src/components/AppResultsSearchForm.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-form class="mt-10 d-flex">
+		<v-container fluid>
+			<h1>Поиск видео</h1>
+			<v-row class="mt-3" no-gutters>
+				<v-col>
+					<v-row no-gutters>
+						<app-search-field-save-favourite />
+					</v-row>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-form>
+</template>
+
+<script>
+import AppSearchFieldSaveFavourite from '@/components/AppSearchFieldSaveFavourite'
+
+export default {
+	name: 'AppResultsSearchForm',
+
+	components: {
+		AppSearchFieldSaveFavourite,
+	},
+}
+</script>
