a6128dfca5a97951bc51b8f6754ca126e60b75c0
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-30 22:41:17 +0700

    feat(save favourite form): создать компонент диалога с формой сохранения запроса

--- src/components/AppSearchFieldSaveFavourite.vue
+++ src/components/AppSearchFieldSaveFavourite.vue
@@ -1,63 +1,19 @@
 <template>
 	<app-search-field-base>
-		<v-menu 
-			content-class="menu"
-			:close-on-content-click="false"
-			offset-y 
-			max-width="220"
-			nudge-left="99"
-			nudge-bottom="8"
-		>
-			<template v-slot:activator="{ on }">
-				<v-icon
-					color="primary"
-					v-on="on"
-				>
-					mdi-heart-outline
-				</v-icon>
-			</template>
-			<v-card>
-				<v-card-text class="black--text">
-					Поиск сохранён в разделе «Избранное»
-				</v-card-text>
-				<v-card-text class="pt-0">
-					<router-link to="favourites">Перейти в избранное</router-link>
-				</v-card-text>
-			</v-card>
-		</v-menu>
+		<app-search-field-save-favourite-dialog />
 	</app-search-field-base>
 </template>
 
 <script>
 import AppSearchFieldBase from '@/components/AppSearchFieldBase'
+import AppSearchFieldSaveFavouriteDialog from '@/components/AppSearchFieldSaveFavouriteDialog'
 
 export default {
 	name: 'AppSearchFieldSaveFavourite',
 
 	components: {
 		AppSearchFieldBase,
+		AppSearchFieldSaveFavouriteDialog,
 	},
 }
 </script>
-
-<style lang="scss" scoped>
-.menu {
-	contain: initial;
-	overflow: visible;
-
-	&:before {
-		position: absolute;
-		content: '';
-		display: block;
-
-		height: 14px;
-		width: 14px;
-		bottom: 100%;
-		left: calc(50% - 7px);
-		z-index: 10;
-
-		border: 7px solid transparent;
-		border-bottom-color: #FFF;
-	}
-}
-</style>
