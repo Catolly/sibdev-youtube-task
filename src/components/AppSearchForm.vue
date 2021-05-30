d94084e914aaf540c52368ff0e5221130e20e205
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-31 06:22:04 +0700

    feat(search form): добавить submit формы с редиректом на страницу результатов

--- src/components/AppSearchForm.vue
+++ src/components/AppSearchForm.vue
@@ -1,11 +1,14 @@
 <template>
-	<v-form class="d-flex fill-height align-center">
+	<v-form 
+		@submit.prevent="submit" 
+		class="d-flex fill-height align-center"
+	>
 		<v-container fluid>
 			<h1 class="text-center">Поиск видео</h1>
 			<v-row class="mt-10" justify="center" no-gutters>
 				<v-col cols="8">
 					<v-row no-gutters>
-						<app-search-field-base />
+						<app-search-field-base v-model="query" @search="submit" />
 					</v-row>
 				</v-col>
 			</v-row>
@@ -14,6 +17,8 @@
 </template>
 
 <script>
+import { RESULTS_ROUTE_NAME } from '@/router/constants'
+
 import AppSearchFieldBase from '@/components/AppSearchFieldBase'
 
 export default {
@@ -21,6 +26,21 @@ export default {
 
 	components: {
 		AppSearchFieldBase,
+	},
+
+	data:() => ({
+		query: '',
+	}),
+
+	methods: {
+		submit() {
+			if (!this.query) return
+
+			this.$router.push({ 
+				name: RESULTS_ROUTE_NAME,
+				query: { search_query: this.query } 
+			})
+		},
 	},
 }
 </script>
