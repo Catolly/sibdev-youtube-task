4bb1f640ffd654ee4e483821acd7067d742a9cc0
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-31 00:03:48 +0700

    feat(favourites page): сверстать страницу избранных

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,20 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/results',
+    name: 'Results',
+    component: () => import(/* webpackChunkName: "Results" */ '@/views/Results')
+  },
+  {
+    path: '/favourites',
+    name: 'Favourites',
+    component: () => import(/* webpackChunkName: "Favourites" */ '@/views/Favourites')
+  },
+  {
     path: '/',
     name: 'Search',
     component: () => import(/* webpackChunkName: "Search" */ '@/views/Search')
   },
-  {
-    path: '/results',
-    name: 'Results',
-    component: () => import(/* webpackChunkName: "Results" */ '@/views/Results')
-  }
 ]
 
 const router = new VueRouter({
