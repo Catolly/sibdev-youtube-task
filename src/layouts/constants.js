aee5ad83c016b275fbf081d96f13659a78a7b8b5
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-31 21:50:53 +0700

    feat(login page): создать страницу логина

---
+++ src/layouts/constants.js
@@ -0,0 +1,2 @@
+export const AUTH_LAYOUT_NAME = "app-auth-layout"
+export const DEFAULT_LAYOUT_NAME = "app-default-layout"
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-container fluid class="fill-height pa-0">
+		<v-row justify="center">
+			<v-col lg="6" md="8" sm="12">
+				<app-login-form />
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+import AppLoginForm from '@/components/AppLoginForm'
+
+export default {
+	name: 'Login',
+
+	components: {
+		AppLoginForm,
+	},
+}
+</script>
