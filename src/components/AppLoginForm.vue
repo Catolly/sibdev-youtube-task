69d100d96f6cd62ab4a5b022875d53b768871266
Catolly <45347427+Catolly@users.noreply.github.com>
2021-06-01 00:31:07 +0700

    feat(login form): подключить store login

--- src/components/AppLoginForm.vue
+++ src/components/AppLoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-form>
+	<v-form @submit.prevent="submit">
 		<v-card>
 			<v-container fluid class="py-10 px-0">
 				<v-row justify="center">
@@ -10,27 +10,38 @@
 								height="88"
 								width="88"
 							/>
-							
+
 							<v-card-title>Вход</v-card-title>
+
+							<v-card-subtitle 
+								v-show="loginError"
+								class="error--text"
+							>
+								Неверный логин или пароль
+							</v-card-subtitle>
 						</div>
 
 						<label>
 							<span class="text--disabled text-truncate">Логин</span>
-							<v-text-field 
+							<v-text-field
+								v-model="form.login"
 								outlined
 								required
 								type="text"
+								@keyup.enter="submit"
 							/>						
 						</label>	
 
 						<label>
 							<span class="text--disabled text-truncate">Пароль</span>
 							<v-text-field 
+								v-model="form.password"
 								:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
 								:type="showPassword ? 'text' : 'password'"
 								outlined
 								required
 								@click:append="toggleShowPassword"
+								@keyup.enter="submit"
 							/>						
 						</label>
 
@@ -38,6 +49,7 @@
 							<v-btn
 								color="primary"
 								x-large
+								@click="submit"
 							>
 								Войти
 							</v-btn>
@@ -50,16 +62,37 @@
 </template>
 
 <script>
+import { LOGIN } from '@/store/actions.type'
+
+import { SEARCH_ROUTE_NAME } from '@/router/constants'
+
 export default {
 	name: 'AppLoginForm',
 
 	data:() => ({
 		showPassword: false,
+		loginError: false,
+
+		form: {
+			login: '',
+			password: '',
+		},
 	}),
 
 	methods: {
 		toggleShowPassword() {
 			this.showPassword = !this.showPassword
+		},
+
+		submit() {
+			try {
+				this.loginError = false
+				this.$store.dispatch(LOGIN, this.form)
+				this.$router.push({ name: SEARCH_ROUTE_NAME })
+			} catch (error) {
+				console.error(error)
+				this.loginError = true
+			}
 		},
 	},
 }
