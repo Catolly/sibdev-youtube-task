1b4679d194afc3a76ddcade0561232762eff3c8f
Catolly <45347427+Catolly@users.noreply.github.com>
2021-05-31 21:50:10 +0700

    feat(auth layout): создать шаблон без топбара

---
+++ src/layouts/AppAuthLayout.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="layout-container fill-height grey lighten-5">
+		<v-main class="fill-height">
+			<slot />
+		</v-main>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AppAuthLayout',
+}
+</script>
+
+<style scoped>
+.layout-container  {
+	padding-left: 200px !important;
+	padding-right: 200px !important;
+}
+</style>
