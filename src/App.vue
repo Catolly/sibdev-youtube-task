76f0b0816190052b0a9d9f7115386615db332a1b
Catolly <45347427+Catolly@users.noreply.github.com>
2021-06-01 00:29:42 +0700

    feat(component layout meta): подключать layout, указанный в мета роута

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <v-app>  
-    <app-default-layout>
+    <component :is="layout"> 
       <router-view />
-    </app-default-layout>
+    </component>
   </v-app>
 </template>
 
 <script>
+import { DEFAULT_LAYOUT_NAME } from '@/layouts/constants'
+
 import AppDefaultLayout from '@/layouts/AppDefaultLayout'
+import AppAuthLayout from '@/layouts/AppAuthLayout'
 
 export default {
   name: 'App',
 
   components: {
     AppDefaultLayout,
+    AppAuthLayout,
   },
-};
+
+  computed: {
+    layout() {
+      return this.$route.meta.layout || DEFAULT_LAYOUT_NAME
+    },
+  },
+}
 </script>
